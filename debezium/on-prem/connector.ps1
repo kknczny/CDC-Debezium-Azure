25bc6b1eb8d4629d351e9c7404e598d51ee41b89
Kuba Konieczny <kuba.konieczny@gmail.com>
2023-05-18 11:49:20 +0200

    connector commands update

--- debezium/on-prem/connector.ps1
+++ debezium/on-prem/connector.ps1
@@ -1,9 +1,13 @@
 # Add Connector
+cd debezium\on-prem
 $JSON = Get-Content '..\register-sqlserver-eh.json' | Out-String
 Invoke-RestMethod http://localhost:8083/connectors/ -Method POST -Body $JSON -ContentType "application/json"     
 
 # List Connectors
 Invoke-RestMethod http://localhost:8083/connectors
+
+# Connector Status
+Invoke-RestMethod http://localhost:8083/connectors/wwi/status
 
 # Delete Connectors
 Invoke-RestMethod http://localhost:8083/connectors/wwi -Method DELETE
