8777caa1399574547ed76d6e56c3f69bd84fd5d5
Kuba Konieczny <kuba.konieczny@gmail.com>
2023-05-29 10:45:48 +0200

    python scripts folder rename

--- python/auto_add_transactions.py
+++ python/auto_add_transactions.py
@@ -26,13 +26,12 @@ def run_query(cursor, query):
     try:
         cursor.execute(query)
         cursor.commit()
-        # print("SQL query execution successful")
     except pyodbc.Error as e:
         raise e
     
 
 # Random int in range 1-5 and then execute one of the queries
-# Wait 10 sec (to check what is average distance between transactions in Summit)
+# Wait 10 sec (<<>>to check what is average distance between transactions in Summit)
     
 def execute(num_reruns = 0, sleep_secs = 10):
     if num_reruns <= 0 or num_reruns > 1000:
@@ -44,9 +43,9 @@ def execute(num_reruns = 0, sleep_secs = 10):
         query_name = f"insert_query_{query_no}"
         query = getattr(insert_queries, query_name)
         run_query(cursor, query)
-        print(f"Query \"{query_name}\" runned. Waiting {sleep_secs} sec(s) for next query execution if awaiting.")
+        print(f"Query \"{query_name}\" runned. Waiting {sleep_secs} sec(s) for next query execution if awaiting ({run}/{num_reruns}).")
         sleep(sleep_secs)
         run += 1
     
 if __name__ == "__main__":
-    execute(num_reruns=5, sleep_secs=2)
+    execute(num_reruns=100, sleep_secs=15)
