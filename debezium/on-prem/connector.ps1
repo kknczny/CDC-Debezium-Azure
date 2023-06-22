cb76f21ee56ec3f2129d02ac3dc7cacf2ef6ede7
Kuba Konieczny <kuba.konieczny@gmail.com>
2023-06-22 20:23:31 +0200

    small config updates

--- debezium/on-prem/connector.ps1
+++ debezium/on-prem/connector.ps1
@@ -1,13 +1,18 @@
 # Add Connector
 cd debezium\on-prem
-$JSON = Get-Content '..\register-sqlserver-eh.json' | Out-String
+$JSON = Get-Content '..\connector_config_orders.json' | Out-String
+$JSON = Get-Content '..\connector_config_stock.json' | Out-String
+
+
 Invoke-RestMethod http://localhost:8083/connectors/ -Method POST -Body $JSON -ContentType "application/json"     
 
 # List Connectors
 Invoke-RestMethod http://localhost:8083/connectors
 
 # Connector Status
-Invoke-RestMethod http://localhost:8083/connectors/wwi-00/status
+Invoke-RestMethod http://localhost:8083/connectors/orders-00/status
+Invoke-RestMethod http://localhost:8083/connectors/stock-00/status
 
 # Delete Connectors
-Invoke-RestMethod http://localhost:8083/connectors/wwi-00 -Method DELETE
+Invoke-RestMethod http://localhost:8083/connectors/orders-00 -Method DELETE
+Invoke-RestMethod http://localhost:8083/connectors/stock-00 -Method DELETE
